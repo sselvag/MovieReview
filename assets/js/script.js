a00c0daabd047d265cd818f8c2084ec506608874
Swaathy <64749332+sselvag@users.noreply.github.com>
2023-08-01 16:53:14 -0400

    Updating html location

--- assets/js/script.js
+++ assets/js/script.js
@@ -193,7 +193,7 @@ function nextPageImage(event){
       localStorage.setItem("historyTitle", JSON.stringify(resultsTitle));
     }
 
-    window.location.href = "./assets/pages/secondPage.html";
+    window.location.href = "./assets/secondPage.html";
     localStorage.setItem("title", event.target.id)
     localStorage.setItem("img", event.target.src)
 }
